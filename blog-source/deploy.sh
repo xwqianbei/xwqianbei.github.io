b52892231c77b6b735113c6f66c10f46b5cf37dc
xw <2816739071@qq.com>
2026-05-23 01:43:51 +0800

    feat: add life moments section and Chinese localization to site navigation

--- blog-source/deploy.sh
+++ blog-source/deploy.sh
@@ -8,10 +8,10 @@ NODE="/Users/xw/.nvm/versions/node/v24.11.0/bin/node"
 HEXO="$SCRIPT_DIR/node_modules/.bin/hexo"
 
 echo "==> Cleaning..."
-$NODE $HEXO clean
+$NODE $HEXO --cwd "$SCRIPT_DIR" clean
 
 echo "==> Generating..."
-$NODE $HEXO generate
+$NODE $HEXO --cwd "$SCRIPT_DIR" generate
 
 echo "==> Copying to root..."
 # List of generated directories/files to copy
